48eeef948af139fabadd1faa36783f2ad3e7cdad
Sidharth K V <102485402+01sidharth@users.noreply.github.com>
2023-12-01 21:14:08 +0530

    sum,fact program updated

--- Datastructure/Factorial_of_n_recursion.java
+++ Datastructure/Factorial_of_n_recursion.java
@@ -1,15 +1,26 @@
 package Datastructure;
 
 public class Factorial_of_n_recursion {
-	static int fact=1;
+//	static int fact=1;
+//	public static int facts(int n)
+//	{
+//		if(n>=1)
+//		{
+//			fact=fact*n;
+//			facts(n-1);	
+//		}
+//		return fact;
+//	}
 	public static int facts(int n)
 	{
-		if(n>=1)
+		if(n==0)
 		{
-			fact=fact*n;
-			facts(n-1);	
+			return 1;
 		}
-		return fact;
+		else
+		{
+			return n*facts(n-1);	
+		}
 	}
 	public static void main(String[] args) {
 		System.out.println(facts(6));
