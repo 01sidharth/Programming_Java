9f4a6257b205923432087acb23a2e861265474c4
Sidharth K V <102485402+01sidharth@users.noreply.github.com>
2023-12-19 18:09:27 +0530

    linked list complete program

--- Datastructure/LinkedListimpl.java
+++ Datastructure/LinkedListimpl.java
@@ -3,7 +3,7 @@ package linkedlist;
 public class LinkedListimpl {
 	Node head;
 	
-	void insert(int ele)
+	void insertlast(int ele)
 	{
 		Node n=new Node(ele);
 		if(head==null)
@@ -19,7 +19,95 @@ public class LinkedListimpl {
 			}
 			temp.next=n;
 		}
-		System.out.println(ele+"had been Inserted..!");
+		System.out.println(ele+":had been Inserted..!");
+	}
+	void insertfrond(int ele)
+	{
+		Node n=new Node(ele);
+		if(head==null)
+		{
+			head=n;
+		}
+		else
+		{
+			n.next=head;
+			head=n;
+		}
+	}
+	void display() {
+		if(head==null)
+		{
+			System.out.println("List is Empty");
+		}
+		else
+		{	System.out.println("LinkedList Data:");
+			Node temp=head;
+			while(temp!=null)
+			{
+				System.out.print(temp.data+" ");
+				temp=temp.next;
+			}
+			System.out.println();
+		}
+	}
+	void deletelast()
+	{
+		if(head==null)
+		{
+			System.out.println("List is Empty");
+		}
+		else if(head.next==null)
+		{
+			System.out.println(head.data+ ":deleted");
+			head=null;
+		}
+		else
+		{
+			Node temp=head;
+			while(temp.next.next!=null)
+			{
+				temp=temp.next;
+			}
+			System.out.println(temp.next.data+":deleted");
+			temp.next=null;
+		}
+	}
+	void deletefrond() {
+		if(head==null)
+		{
+			System.out.println("LinkedList is Empty");
+		}
+		else
+		{
+			System.out.println(head.data+":deleted");
+			head=head.next;
+		}
+	}
+	void delete(int ele)
+	{
+		if(head==null)
+		{
+			System.out.println("LinkedList is Empty");
+		}
+		else if(head.data==ele)
+		{
+			System.out.println(ele+":deleted");
+			head=head.next;
+		}
+		else
+		{
+			Node temp=head;
+			while(temp.next!=null)
+			{   if(temp.next.data==ele)
+				{
+				System.out.println(temp.next.data+":deleted");
+				temp.next=temp.next.next;
+				return;
+				}
+			    temp=temp.next;
+				
+			}
+		}
 	}
 
 }
--- Datastructure/Runner.java
+++ Datastructure/Runner.java
@@ -1,10 +1,80 @@
 package linkedlist;
 
+import java.util.Scanner;
+
 public class Runner {
  public static void main(String[] args) {
 	LinkedListimpl li=new LinkedListimpl();
-	li.insert(5);
-	li.insert(10);
-	li.insert(15);
+//	li.display();
+//	li.insertlast(5);
+//	li.insertlast(10);
+//	li.insertlast(15);
+//	li.display();
+//	li.insertlast(36);
+//	li.display();
+//	li.deletelast();
+//	li.display();
+//	li.deletefrond();
+//	li.display();
+//	li.deletefrond();
+//	li.display();
+//	li.insertfrond(100);
+//	li.display();
+//	li.insertfrond(200);
+//	li.display();
+//	li.insertlast(300);
+//	li.display();
+//	li.deletefrond();
+//	li.display();
+//	li.deletelast();
+//	li.display();
+//	li.insertfrond(1000);
+//	li.insertfrond(1060);
+//	li.insertfrond(3000);
+//	li.display();
+//	li.delete(1000);
+//	li.display();
+//	li.delete(3000);
+//	li.display();
+	Scanner sc=new Scanner(System.in);
+	System.out.println("=============LinkedList Operation===================");
+	while(true)
+	{
+		System.out.println("=============MENU===============");
+		System.out.println("1.insert data\t 2.insertLast \t 3.insertFrond \t4.DeleteLast \t5.DeleteFrond\t6.DeleteData \t7.Display\t8.exit");
+		int op=sc.nextInt();
+		switch(op)
+		{
+		case 1:System.out.println("Enter the Element to Insert:");
+		int ele=sc.nextInt();
+		li.insertlast(ele);
+		break;
+		case 2:System.out.println("Enter the Element to Insert at Last");
+		int ele1=sc.nextInt();
+		li.insertlast(ele1);
+		break;
+		case 3:System.out.println("Enter the Element to Insert at Frond");
+		int ele2=sc.nextInt();
+		li.insertfrond(ele2);
+		break;
+		case 4:li.deletelast();break;
+		case 5:li.deletefrond();break;
+		case 6:System.out.println("Enter the Element to delete:");
+		int ele3=sc.nextInt();
+		li.delete(ele3);
+		break;
+		case 7:li.display();break;
+		case 8:System.out.println("Successfully Exited :)");
+		System.exit(0);
+		break;
+		default:System.out.println("Enter the valid Option");
+		break;
+		}
+	}
+	
+	
+
+
+	
 }
 }
